6358c39a35ae891f7459f2276abc25593d9df272
Evgeniy Degtyar <farappl@gmail.com>
2022-08-21 15:46:30 +0500

    fix timezone in 'from_date'

--- task3.py
+++ task3.py
@@ -1,12 +1,13 @@
 import requests
 from pprint import pprint
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 def get_questions_on_stack_stackoverflow(days_shift, tag):
     to_date = int(datetime.timestamp(datetime.today()))
     from_date = int(datetime.timestamp(datetime(datetime.today().year,
-                                                datetime.today().month, datetime.today().day - days_shift, 5)))
+                                                datetime.today().month, datetime.today().day - days_shift,
+                                                tzinfo=timezone.utc)))
     url = 'https://api.stackexchange.com/2.3/questions'
     params = {'fromdate': from_date,
               'todate': to_date,
